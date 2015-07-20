a420e06127cc53b007721f97d5cd5087dc11ed35
Marlyn Olivo <winmoe58@hotmail.com>
2015-07-20 18:15:53 -0430

    Despliegue DB en Heroku prueba2

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -2,18 +2,22 @@ var models = require('../models/models.js');
 
 //GET/quizes/show
 exports.show= function(req,res){
-		res.render('quizes/show',{pregunta:'Capital de Italia'});
+	models.Quiz.findAll().then(function(quiz){
+		res.render('quizes/show',{pregunta:quiz[0].pregunta})
+	})
 };
 
 //GET/quizes/answer
 exports.answer= function(req,res){
-	if(req.query.respuesta ==='Roma'){
-		res.render('quizes/answer',
+	models.Quiz.findAll().then(function(quiz){
+		if(req.query.respuesta === quiz[0].respuesta){
+			res.render('quizes/answer',
 			{respuesta:'Su respuesta es correcta'});
-	}else{
-		res.render('quizes/answer',
+		}else{
+			res.render('quizes/answer',
 			{respuesta: 'Su respuesta es incorrecta,verifique mayúsculas y miúsculas'});
-	}
+		}
+	})
 };
 
 
--- models/models.js
+++ models/models.js
@@ -1,12 +1,28 @@
 var path = require('path');
-var Sequelize = require('sequelize');
+//Postgres = DATABASE_URL = postgres://fxnakxgenzkfod:eETa0d-qNrMG0GucYzR5i5nFWg@ec2-54-235-145-226.compute-1.amazonaws.com:5432/d41h6b325sj764
+//Sqlite DATABASE_URL = sqlite://:@:/
+var url = process.env.DATABASE_URL.match(/(.*)\:\/\/(.*?)\:(.*)@(.*)\:(.*)\/(.*)/);
+var DB_name  = (url[6]||null);
+var user     = (url[2]||null);
+var pwd      = (url[3]||null);
+var protocol = (url[1]||null);
+var dialect  = (url[1]||null);
+var port     = (url[5]||null);
+var host     = (url[4]||null);
+var storage  = process.env.DATABASE_STORAGE;
 
-var sequelize = new Sequelize(null, null, null, 
-  { dialect: "sqlite", storage:"quiz.sqlite"}      
+var Sequelize = require('sequelize');
+var sequelize = new Sequelize(DB_name, user, pwd, 
+  { dialect:  protocol,
+    protocol: protocol,
+    port:     port,
+    host:     host,
+    storage:  storage,  // solo SQLite (.env)
+    omitNull: true      // solo Postgres
+  }      
 );
 
 var Quiz = sequelize.import(path.join(__dirname,'quiz'));
-
 exports.Quiz = Quiz; 
 
 sequelize.sync().then(function(){
