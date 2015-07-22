ebe0406ac35f2a614090fe73c4ad197cb2e0a4e9
Marlyn Olivo <winmoe58@hotmail.com>
2015-07-22 17:42:57 -0430

    Actualizacion, layout no funciona

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -3,13 +3,13 @@ var models = require('../models/models.js');
 //GET/quizes/
 exports.index= function(req,res){
 		models.Quiz.findAll().then(function(quizes){
-		res.render('quizes/index',{quizes:quizes});
+		res.render('quizes/index.ejs', {quizes: quizes});
 	})
 };
 
 //GET/quizes/:Id
 exports.show= function(req,res){
-		models.Quiz.find(req.params.quizId).then(function(quiz){
+		models.Quiz.find(req.params.quizId).then(function(quiz) {
 		res.render('quizes/show',{quiz: quiz});
 	})
 };
